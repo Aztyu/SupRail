495b066b25c5d976450dc0f7dcacfe9ec6b06a67
Guillaume Oriol <oriol.guillaume42@gmail.com>
2016-06-06 09:35:31 +0200

    Fix problem Jsondata

--- src/main/webapp/resources/js/jsondata.js
+++ src/main/webapp/resources/js/jsondata.js
@@ -5,41 +5,7 @@ $(document).ready(function(){
             $('input.chooseTravel[name="check1"]').click(function(e){
                 $('input.chooseTravel[name!="check1"]').prop('checked',false);
             });  
-	  // Get the form fields and hidden div
-	  var checkbox1 = $("#check1");
-	  var checkbox2 = $("#check2");
-	  var hidden = $("clock_end");
-	  
-	  hidden.show();
-	  
-	  // Setup an event listener for when the state of the 
-	  // checkbox changes.
-	  checkbox1.change(function() {
-	    // Check to see if the checkbox is checked.
-	    // If it is, show the fields and populate the input.
-	    // If not, hide the fields.
-	    if (checkbox2.is(':checked')) {
-	      // Show the hidden fields.
-	      hidden.hide();
-	      // Populate the input.
-	      
-	    } else {
-	      // Make sure that the hidden fields are indeed
-	      // hidden.
-	      hidden.show();
-	      
-	      // You may also want to clear the value of the 
-	      // hidden fields here. Just in case somebody 
-	      // shows the fields, enters data to them and then 
-	      // unticks the checkbox.
-	      //
-	      // This would do the job:
-	      //
-	      // $("#hidden_field").val("");
-	    }
-	  });
-	});
-
+            
 var options = {
 	
 			    url: "http://localhost:8080/api/stations",
